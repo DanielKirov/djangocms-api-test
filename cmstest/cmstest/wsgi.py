671fd2fa7111ff197d2ca7d7233d4bdbbe8cf224
Daniel Kirov <daniel.kirov@PANMAC022-2.local>
2017-01-25 16:10:08 +0000

    Add rest api to path.

--- cmstest/cmstest/wsgi.py
+++ cmstest/cmstest/wsgi.py
@@ -6,8 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
+import sys
 import os
+sys.path.insert(0, 'djangocms-rest-api/')
 
 from django.core.wsgi import get_wsgi_application
 
